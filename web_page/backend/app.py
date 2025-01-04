4957610100dfc10aa1b520f7d7296d52358a310a
alex garcia montane <alex@talentup.io>
2025-01-04 14:23:01 +0100

    try

--- web_page/backend/app.py
+++ web_page/backend/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+app = Flask(__name__,
+            template_folder=os.path.join(os.getcwd(), 'frontend'),
+            static_folder=os.path.join(os.getcwd(), 'frontend', 'static'))
 
 # Ruta principal (home)
-@app.route("/frontend/")
+@app.route("/")
 def home():
     return render_template("index.html")
 
@@ -13,4 +16,4 @@ def about():
     return "Esta es la página 'About'."
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+    app.run(debug=True)
