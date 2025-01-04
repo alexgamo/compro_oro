a429baddd384e62818a558af6389c32c301da957
alex garcia montane <alex@talentup.io>
2025-01-04 14:16:16 +0100

    change

--- web_page/backend/app.py
+++ web_page/backend/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Ruta principal (home)
-@app.route("/")
+@app.route("/frontend/")
 def home():
     return render_template("index.html")
 
