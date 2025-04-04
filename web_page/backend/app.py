6751d714b5f279e3e8ca4257216d624971dd051e
alex garcia montane <alex@talentup.io>
2025-04-04 18:11:45 +0200

    changes

--- web_page/backend/app.py
+++ web_page/backend/app.py
@@ -1,21 +1,68 @@
-from flask import Flask, render_template, redirect, request
 import os
+from flask import Flask, render_template, redirect, jsonify
+import requests
+import datetime
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+API_KEY = '0Q1V8UQTYXP825ZW'
+GOLD_URL = 'https://www.alphavantage.co/query'
 
 app = Flask(__name__,
-            template_folder=os.path.join(os.getcwd(), 'frontend'),
-            static_folder=os.path.join(os.getcwd(), 'frontend', 'static'))
+            template_folder=os.path.join(BASE_DIR, 'frontend'),
+            static_folder=os.path.join(BASE_DIR, 'frontend', 'static'))
 
-# Ruta principal (home)
 @app.route("/")
 def home():
-    if request.host.startswith("www."):
-        return redirect("https://lacentraldeloro.com", code=301)
+    #if request.host.startswith("www."):
+    #    return redirect("https://lacentraldeloro.com", code=301)
     return render_template("index.html")
 
-# Ruta de ejemplo
-@app.route("/about")
-def about():
-    return "Esta es la página 'About'."
+@app.route('/compro_oro_plata')
+def compro_oro_y_plata():
+    return render_template('compro_oro_plata.html')
+
+@app.route('/casa_empeños')
+def casa_empeños():
+    return render_template('casa_empeños.html')
+
+@app.route('/oro_inversion')
+def oro_inversion():
+    return render_template('oro_inversion.html')
+
+@app.route('/gold-data')
+def gold_data():
+    params = {
+        'function': 'TIME_SERIES_MONTHLY',
+        'symbol': 'XAUUSD',  # Oro frente al dólar
+        'apikey': API_KEY
+    }
+
+    response = requests.get(GOLD_URL, params=params)
+    data = response.json()
+
+    if 'Monthly Time Series' not in data:
+        return jsonify({'error': 'No se pudieron obtener datos'}), 500
+
+    time_series = data['Monthly Time Series']
+    sorted_dates = sorted(time_series.keys(), reverse=True)[:60]  # Últimos 10 años (120 meses)
+
+    history = [{
+        'date': date,
+        'price': float(time_series[date]['4. close'])
+    } for date in sorted(sorted_dates)]  # Ordenado cronológicamente
+
+    return jsonify(history)
+
+@app.route('/venta_joyas')
+def venta_joyas():
+    return render_template('venta_joyas.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+# compro oro. collar principal, anillos, efectivo, apreton de manos sin pistolas
+# empeños. reloj fondo. taller, calendario, caja fuerte
+# oro inversion. bolsa, oro
+# venta joyas. escaparate joyas, chica joya, caja joyas
